37f0cfca8e379f7503045f26adee5421bc9206ad
catherine <catherine.bacon@gmail.com>
2017-03-20 16:41:03 +0000

    convert trader books to dropdown selection

--- imports/ui/Trader.jsx
+++ imports/ui/Trader.jsx
@@ -7,14 +7,33 @@ import Book from './Book.jsx';
 
 
 export default class Trader extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 'Decline Trade',
+    };
+  }
 
   renderTraderBooks() {
     let traderBooks = this.props.traderBooks;
     return traderBooks.map( book => (
-      <li key={book._id}>
+      <option key={book._id} value={book._id}>
         {book.title} by {book.author}
-      </li>
+      </option>
     ));
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    /// submit should 1. if declined on traded book - tradeProposed=false and proposedByUsername, proposedById reset to ''
+    // 2. should change book to exchanged = true, list exchange in both users 'Successful Trades list', 
+    // book not show on all books list, email users
+  }
+
+  handleChange() {
+    this.setState({
+      value: event.target.value,
+    });
   }
 
   render() {
@@ -24,13 +43,16 @@ export default class Trader extends Component {
       );
     }
     return (
-      <span>
-        Trade proposed by {this.props.book.proposedByUsername}
-        <ul>
-          <li>Decline trade</li>
-          {this.renderTraderBooks()}
-        </ul>
-      </span>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label> 
+            Trade proposed by {this.props.book.proposedByUsername}
+          </label>
+          <select value={this.state.value} onChange={this.handleChange.bind(this)}>
+            <option key='decline' value='Decline Trade'>Decline Trade</option>
+            {this.renderTraderBooks()}
+          </select>
+          <input type="submit" value="Go!" />
+        </form>
     );
   }
 }
@@ -39,3 +61,4 @@ Trader.propTypes = {
   book: PropTypes.object.isRequired,
   traderBooks: PropTypes.array.isRequired,
 };
+
