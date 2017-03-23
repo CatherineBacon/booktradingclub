4a782bcbfdf71726fc7180c2b997296f2662e9d0
Catherine Bacon <catherine@Catherines-MacBook.local>
2017-03-23 19:56:45 +0000

    move links to be avavailable on all pages

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -13,31 +13,45 @@ import AllBooks from './AllBooks.jsx';
 import Profile from './Profile.jsx';
 
 class App extends Component {
-
   render() {
     return (
       <Router>
-        <div className='container'>
+        <div className="container">
           <header>
             <h1>Book Exchange!</h1>
           </header>
 
           <AccountsUIWrapper />
-
-          <Route exact path='/' render={ () => (
+          {Meteor.userId() &&
             <div>
-              <h1>Welcome to Book Exchange!</h1>
-              <Link to='/mybooks'>My Books</Link>
-              <Link to='/allbooks'>All Books</Link>
-              <Link to='/profile'>Profile</Link>
-            </div>
-          )} />
+              <Link to="/">Home</Link>
+              <Link to="/mybooks">My Books</Link>
+              <Link to="/allbooks">All Books</Link>
+              <Link to="/profile">Profile</Link>
+            </div>}
 
-          <Route path='/profile' component={Profile} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <div>
+                <h1>Welcome to Book Exchange!</h1>
+                <ul>
+                  <li>List all your books</li>
+                  <li>Find a book you'd like to read</li>
+                  <li>
+                    Arrange book exchanges with users from all over the world!
+                  </li>
+                </ul>
+              </div>
+            )}
+          />
+
+          <Route path="/profile" component={Profile} />
 
           <Route path="/mybooks" component={MyBooks} />
 
-          <Route path='/allbooks' component={AllBooks} />
+          <Route path="/allbooks" component={AllBooks} />
 
         </div>
       </Router>
@@ -47,15 +61,21 @@ class App extends Component {
 
 App.propTypes = {
   books: PropTypes.array.isRequired,
-  currentUser: PropTypes.object,
+  currentUser: PropTypes.object
 };
 
-export default createContainer(() => {
-  Meteor.subscribe('books');
+export default createContainer(
+  () => {
+    Meteor.subscribe('books');
 
-  return {
-    books: Books.find({}, {
-      sort: { createdAt: -1 }
-    }).fetch(),
-  };
-}, App);
+    return {
+      books: Books.find(
+        {},
+        {
+          sort: { createdAt: -1 }
+        }
+      ).fetch()
+    };
+  },
+  App
+);
