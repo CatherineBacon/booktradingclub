911d686a27c71ca14e325fd7f3a260ef080a73f5
Catherine Bacon <catherine@Catherines-MacBook.local>
2017-03-23 17:02:15 +0000

    add user profile form

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,35 +1,40 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
-if(Meteor.isServer && this.userId) {
-	Meteor.publish('Meteor.users.additionalinfo', function () {
-		return Meteor.users.find(
-			{ _id: this.userId },
-			{ fields: {
-				'fullName': 1,
-				'city': 1,
-				'country': 1,
-			} }
-		)
-	});
+if (Meteor.isServer) {
+  Meteor.publish('Meteor.users.additionalinfo', function() {
+    return Meteor.users.find(
+      { _id: this.userId },
+      {
+        fields: {
+          fullName: 1,
+          city: 1,
+          country: 1
+        }
+      }
+    );
+  });
 }
 
-
 Meteor.methods({
-	'Meteor.users.additionalinfo.update'(fullName, city, country){
-		check(fullName, String);
-		check(city, String);
-		check(country, String);
+  'Meteor.users.additionalinfo.update'(fullName, city, country) {
+    check(fullName, String);
+    check(city, String);
+    check(country, String);
 
-		if(!Meteor.userId()){
-			throw new Meteor.Error('not-authorized')
-		}
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
 
-		Meteor.users.additionalinfo.update({_id: Meteor.user()._id}, 
-			{$set: {
-				fullName: fullName,
-				city: city,
-				country: country,
-			}}
-		)
-	}
-})
+    Meteor.users.update(
+      { _id: Meteor.user()._id },
+      {
+        $set: {
+          fullName: fullName,
+          city: city,
+          country: country
+        }
+      }
+    );
+  }
+});
--- imports/ui/Profile.jsx
+++ imports/ui/Profile.jsx
@@ -6,77 +6,89 @@ import { createContainer } from 'meteor/react-meteor-data';
 import UserInfo from '../api/users.js';
 
 class Profile extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			fullName: '',
-			city: '',
-			countrty: '',
-		}
+    this.state = {
+      fullName: '',
+      city: '',
+      country: ''
+    };
 
-		this.handleInputChange = this.handleInputChange.bind(this);
-	}
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-	handleInputChange(event) {
-		const target = event.target;
-		const value = target.value;
-		const name = target.name;
-		
-		this.setState({
-			[name]: value,
-		}) 
-	}
+  handleInputChange(event) {
+    const { value, name } = event.target;
 
-	handleSubmit(event) {
-		event.preventDefault();
+    this.setState({
+      [name]: value
+    });
+  }
 
-		const fullName = this.state.fullName;
-		const city = this.state.city;
-		const country = this.state.country;
+  handleSubmit(event) {
+    event.preventDefault();
 
-		Meteor.call('Meteor.users.additionalinfo.update', fullName, city, country);
-	}
-	
-	render() {
-		if(this.props.currentUser){
-			return (
-				<div>
-					<h2>Profile</h2>
-	       			<p><b>username</b>: {Meteor.user().username}</p>
-	       			<p><b>email address</b>: {Meteor.user().emails[0].address}</p>
+    const { fullName, city, country } = this.state;
 
-	       			<h4>Update Profile</h4>
-	       			<form>
-	       				<label>
-	       					Full name:
-	       					<input type='text' name='fullName' onChange={this.handleInputChange} value={this.state.fullName}/>
-	       				</label>
-	       				<label>
-	       					Town/City:
-	       					<input type='text' name='city' onChange={this.handleInputChange} value={this.state.city}/>
-	       				</label>
-	       				<label>
-	       					Country:
-	       					<input type='text' name='country' onChange={this.handleInputChange} value={this.state.country}/>
-	       				</label>
-	       				<input type='submit' value='Submit' onSubmit={this.handleSubmit}/>
-	       			</form>
-	       			{console.log(Meteor.user())}
-				</div>
-			);
-		} else {
-			return <div>Please login</div>
-		}
-		
-	}
+    Meteor.call('Meteor.users.additionalinfo.update', fullName, city, country);
+  }
+
+  render() {
+    if (!this.props.currentUser) return <div>Please login</div>;
+
+    const { fullName } = this.props.currentUser;
+
+    return (
+      <div>
+        <h2>Profile</h2>
+        <p><b>username</b>: {Meteor.user().username}</p>
+        <p><b>email address</b>: {Meteor.user().emails[0].address}</p>
+        <p>{fullName}</p>
+
+        <h4>Update Profile</h4>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>
+            Full name:
+            <input
+              type="text"
+              name="fullName"
+              onChange={this.handleInputChange}
+              value={this.state.fullName}
+            />
+          </label>
+          <label>
+            Town/City:
+            <input
+              type="text"
+              name="city"
+              onChange={this.handleInputChange}
+              value={this.state.city}
+            />
+          </label>
+          <label>
+            Country:
+            <input
+              type="text"
+              name="country"
+              onChange={this.handleInputChange}
+              value={this.state.country}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
+export default createContainer(
+  () => {
+    Meteor.subscribe('Meteor.users.additionalinfo');
 
-export default createContainer(() => {
-	Meteor.subscribe('Meteor.users.additionalinfo');
-
-  return {
-    currentUser: Meteor.user(),
-  };
-}, Profile);
+    return {
+      currentUser: Meteor.user()
+    };
+  },
+  Profile
+);
