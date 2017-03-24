bb74f00ad62f330521221873fde690a0717cb6fa
Catherine Bacon <catherine@Catherines-MacBook.local>
2017-03-24 16:43:27 +0000

    refactor mybooks

--- imports/ui/MyBooks.jsx
+++ imports/ui/MyBooks.jsx
@@ -9,141 +9,165 @@ import Book from './Book.jsx';
 import Trader from './Trader.jsx';
 
 class MyBooks extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			onlyShowProposed: false,
-		};
-	}
+    this.state = {
+      onlyShowProposed: false
+    };
+  }
 
-	handleSubmit(event) {
-	   event.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
 
-	  const title = ReactDOM.findDOMNode(this.refs.titleInput).value.trim();
-	  const author = ReactDOM.findDOMNode(this.refs.authorInput).value.trim();
+    const title = ReactDOM.findDOMNode(this.refs.titleInput).value.trim();
+    const author = ReactDOM.findDOMNode(this.refs.authorInput).value.trim();
 
-	  this.addBook(title, author);
+    this.addBook(title, author);
 
-	  // clear form
-	  ReactDOM.findDOMNode(this.refs.titleInput).value = '';
-	  ReactDOM.findDOMNode(this.refs.authorInput).value = '';
-	}
+    // clear form
+    ReactDOM.findDOMNode(this.refs.titleInput).value = '';
+    ReactDOM.findDOMNode(this.refs.authorInput).value = '';
+  }
 
-	addBook(title, author) {
+  addBook(title, author) {
     Meteor.call('books.insert', title, author);
     //also update user with books they own??
   }
 
   toggleOnlyShowProposed() {
-  	this.setState({
-  		onlyShowProposed: !this.state.onlyShowProposed,
-  	});
+    this.setState({
+      onlyShowProposed: !this.state.onlyShowProposed
+    });
   }
 
   renderYourRequests() {
-  	let books = this.props.books;
-  	let filteredBooks = books.filter( book => book.proposedById==Meteor.userId());
-  	return filteredBooks.map( book => {
-  		return (
-  			<li key={book._id}>
-  				<Book book={book} />
-  			</li>
-  		)
-  	});
+    let books = this.props.books;
+    let filteredBooks = books.filter(
+      book => book.proposedById == Meteor.userId()
+    );
+    return filteredBooks.map(book => {
+      return (
+        <li key={book._id}>
+          <Book book={book} />
+        </li>
+      );
+    });
   }
 
-  renderBooks(onlyShowProposed=this.state.onlyShowProposed) {
-   	let books = this.props.books;
-   	let filteredBooks = books.filter( book => book.owner==Meteor.userId());
+  renderTradeRequests() {
+    let books = this.props.books;
+    let filteredBooks = books.filter(
+      book => book.owner == Meteor.userId() && book.tradeProposed
+    );
 
-   	if(onlyShowProposed){
-   		filteredBooks = filteredBooks.filter( book => book.tradeProposed);
-   	}
+    return filteredBooks.map(book => {
+      let traderBooks = books.filter(b => book.proposedById == b.owner);
 
-   	return filteredBooks.map((book) => {
-   		
-   		let traderBooks = books.filter( b => book.proposedById==b.owner );
-   		
-   		return (
-   			<li key={book._id}>
-     			<Book book={book} page="MyBooks" />
-     			<Trader book={book} traderBooks={traderBooks} />
-     		</li>
-     	)
-   	});
- 	}
+      return (
+        <li key={book._id}>
+          <Book book={book} page="MyBooks" />
+          <Trader book={book} traderBooks={traderBooks} />
+        </li>
+      );
+    });
+  }
 
-	render() {
-		Meteor.subscribe('books');
-		if(this.props.currentUser) {
-			return (
-				<div>
-					<h2>My Books</h2>
+  renderBooks() {
+    let books = this.props.books;
+    let filteredBooks = books.filter(book => book.owner == Meteor.userId());
 
-        	<h4>Add book</h4>
-        	<form className="new-book" onSubmit={this.handleSubmit.bind(this)} >
-         		<input
-	         		type='text'
-	         		ref='titleInput'
-	         		placeholder='Title'
-	       		/>
-	     			<input
-	       			type='text'
-	       			ref='authorInput'
-	       			placeholder='Author'
-	     			/>
-	     			<input
-	       			type='submit'
-	       			value='Add'
-      			/>
-	     		</form>
+    if (this.state.onlyShowProposed) {
+      filteredBooks = filteredBooks.filter(book => book.tradeProposed);
+    }
 
-					<h4>Your trade requests ({this.props.youProposedTradeCount} outstanding)</h4>
-					<ul>{this.renderYourRequests()}</ul>
+    return filteredBooks.map(book => {
+      return (
+        <li key={book._id}>
+          <Book book={book} page="MyBooks" />
+        </li>
+      );
+    });
+  }
 
-					<h4>Trade requests for your books ({this.props.tradeProposedCount} waiting)</h4>
-					<ul>{this.renderBooks(true)}</ul>
+  render() {
+    Meteor.subscribe('books');
+    if (this.props.currentUser) {
+      return (
+        <div>
+          <h2>My Books</h2>
 
-	     		<h4>My books</h4>
-	     		<label className='onlyShowProposed'>
-			      <input
-			        type='checkbox'
-			        readOnly
-			        checked={this.state.onlyShowProposed}
-			        onClick={this.toggleOnlyShowProposed.bind(this)}
-			      />
-		        Only show books where a trade has been proposed
-		      </label>
-	     		<ul>
-	     			{this.renderBooks()}
-	     		</ul>
+          <h4>Add book</h4>
+          <form className="new-book" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" ref="titleInput" placeholder="Title" />
+            <input type="text" ref="authorInput" placeholder="Author" />
+            <input type="submit" value="Add" />
+          </form>
 
-	     		<h4>Successful trades</h4>
-	     		<div>LIST SUCCESFUL TRADES HERE</div>
-				</div>
-			);
-		} else {
-			return <div>Please login</div>
-		}
-	}
+          <h4>
+            Your trade requests ({this.props.youProposedTradeCount} outstanding)
+          </h4>
+          <ul>{this.renderYourRequests()}</ul>
+
+          <h4>
+            Trade requests for your books (
+            {this.props.tradeProposedCount}
+            {' '}
+            waiting)
+          </h4>
+          <ul>{this.renderTradeRequests()}</ul>
+
+          <h4>My books</h4>
+          <label className="onlyShowProposed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.onlyShowProposed}
+              onClick={this.toggleOnlyShowProposed.bind(this)}
+            />
+            Only show books where a trade has been proposed
+          </label>
+          <ul>
+            {this.renderBooks()}
+          </ul>
+
+          <h4>Successful trades</h4>
+          <div>LIST SUCCESFUL TRADES HERE</div>
+        </div>
+      );
+    } else {
+      return <div>Please login</div>;
+    }
+  }
 }
 
 MyBooks.propTypes = {
   books: PropTypes.array.isRequired,
   tradeProposedCount: PropTypes.number.isRequired,
-  youProposedTradeCount: PropTypes.number.isRequired,
+  youProposedTradeCount: PropTypes.number.isRequired
 };
 
-export default createContainer(() => {
-	Meteor.subscribe('books');
+export default createContainer(
+  () => {
+    Meteor.subscribe('books');
 
-  return {
-    books: Books.find({}, {
-      sort: { createdAt: -1 }
-    }).fetch(),
-    tradeProposedCount: Books.find({ tradeProposed: { $ne: false }, owner: Meteor.userId() }).count(),
-    youProposedTradeCount: Books.find({ tradeProposed: { $ne: false }, proposedById: Meteor.userId() }).count(),
-    currentUser: Meteor.user(),
-  };
-}, MyBooks);
+    return {
+      books: Books.find(
+        {},
+        {
+          sort: { createdAt: -1 }
+        }
+      ).fetch(),
+      tradeProposedCount: Books.find({
+        tradeProposed: { $ne: false },
+        owner: Meteor.userId()
+      }).count(),
+      youProposedTradeCount: Books.find({
+        tradeProposed: { $ne: false },
+        proposedById: Meteor.userId()
+      }).count(),
+      currentUser: Meteor.user()
+    };
+  },
+  MyBooks
+);
